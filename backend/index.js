c946b8db9d75f154e4cecce2e403d476e3d3873d
Atharva-Pawar <atharvapawar846@gmail.com>
2026-07-02 23:42:22 +0530

    add router and jwt

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const { router } = require("./routers");
+
+app.use(cors());
+app.use(express.json());
+app.use();
+
+router.use("/api/v1", router)
 
 app.listen(3000);
