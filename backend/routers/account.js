8778211223bcfa5bfa159f8e992ca013ae8588e4
Atharva-Pawar <atharvapawar846@gmail.com>
2026-07-10 22:53:50 +0530

    getting account balance

--- backend/routers/account.js
+++ backend/routers/account.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const { Account } = require("../db");
 const app = express();
 const accountRouter = express.Router();
+
+accountRouter.get("/balance", async (req, res) => {
+  const account = await Account.findOne({
+    userId: req.userId,
+  });
+
+  res.json({
+    balance: account.balance,
+  });
+});
 
 module.exports = {
   accountRouter,
