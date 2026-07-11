6dab31addd202ad6d5d1d156ad3f36c2179c0130
Atharva-Pawar <126759400+Atharva-Pawar@users.noreply.github.com>
2026-07-11 23:45:02 +0530

    Feature/account (#12)
    * getting account balance
    
    * add transfer functionality

--- backend/routers/account.js
+++ backend/routers/account.js
@@ -1,15 +1,72 @@
 const express = require("express");
 const { Account } = require("../db");
 const app = express();
+const { authMiddleware } = require("../middlewares/middleware");
 const accountRouter = express.Router();
 
-accountRouter.get("/balance", async (req, res) => {
+accountRouter.get("/balance",authMiddleware, async (req, res) => {
   const account = await Account.findOne({
     userId: req.userId,
   });
 
   res.json({
     balance: account.balance,
+  });
+});
+
+accountRouter.post("/transfer", authMiddleware, async (req, res, next) => {
+  const session = await mongoose.startSession();
+
+  session.startTransaction();
+
+  const { ammount, to } = req.body;
+
+  const account = await Account.findOne({
+    userId: req.userId,
+  }).session(session);
+
+  if (!account || account.balance < ammount) {
+    res.status(400).json({
+      msg: "Insufficient Balance",
+    });
+  }
+
+  const toAccount = await Account.findOne({
+    userId: to,
+  }).session(session);
+
+  if (!toAccount) {
+    res.status(400).json({
+      msg: "Invalid Account",
+    });
+  }
+
+  await Account.updateOne(
+    {
+      userId: req.userId,
+    },
+    {
+      $inc: {
+        balance: -ammount,
+      },
+    },
+  ).session(session);
+
+  await Account.updateOne(
+    {
+      userId: to,
+    },
+    {
+      $inc: {
+        balance: ammount,
+      },
+    },
+  ).session(session);
+
+  await session.commitTransaction();
+
+  res.json({
+    msg: "Transfer Successful",
   });
 });
 
